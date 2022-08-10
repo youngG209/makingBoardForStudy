d245c830a8cf424e8b09b2b3a6ecd3f538ed3b45
이영균 <younggyoun.lee@fassto.ai>
2022-08-10 16:30:12 +0900

    #7 - 스프링 부트 프로젝트 시작하기

---
+++ src/main/java/com/makingboardforstudy/MakingBoardForStudyApplication.java
@@ -0,0 +1,13 @@
+package com.makingboardforstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakingBoardForStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakingBoardForStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makingboardforstudy/MakingBoardForStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makingboardforstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakingBoardForStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
